f2aea1e0d0878289e08c43421fd734dd08d162c0
Ashish <akarhade5@gmail.com>
2020-04-15 23:23:15 +0530

    Notebook added

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,29 @@
+# Libraries needed for Tensorflow processing
 import tensorflow as tf
+import numpy as np
 import keras
+import random
+import json
+import nltk
+nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
 
 
+with open('intents.json') as jsonfile:
+    intents = json.load(jsonfile)
+
+words = []
+classes = []
+documents = []
+ignore = ['?']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(words)
